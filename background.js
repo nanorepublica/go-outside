86060845a7c51d10ee6969be211b4f44db0c4912
Andrew Miller <andrew.miller@founders4schools.org.uk>
2018-10-23 10:54:59 +0100

    incognito mode now works

--- background.js
+++ background.js
@@ -1,8 +1,29 @@
 
+const hostList = [
+    'newtab',
+]
+
+const getHostListRegex = function() {
+    return hostList.map(function(host) {
+        return '*://' + host + '/*'
+    })
+}
+
+const getHost = function(url) {
+    var a = document.createElement("a");
+    a.href = url
+    return a.hostname
+}
 
 chrome.tabs.onCreated.addListener(function(tab) {
-    console.log(tab)
-    if (tab.incognito) {
-        tab.url = 'https://google.com'
+    const newTabHost = getHost(tab.url)
+    if (hostList.includes(newTabHost) && tab.incognito) {   
+        chrome.tabs.update(tab.id, {
+            url: chrome.runtime.getURL('src/templates/newtab.html'),
+            active: true
+        }, function(tab) {
+            console.log('Updated')
+        })
+        chrome.windows.update(tab.windowId, {focused:true})
     }
 })
